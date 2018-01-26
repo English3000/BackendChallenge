082929fc75c356f323c2d77d614c96dea1ee78f7
Alexander <english3000.org@gmail.com>
2018-01-26 09:22:16 -0800

    ...

--- frontend/reducers/errorsReducer.js
+++ frontend/reducers/errorsReducer.js
@@ -1,10 +1,10 @@
-import { RECEIVE_ERRORS } from '../actions/sessionActions';
+// import { RECEIVE_ERRORS } from '../actions/sessionActions';
 
 export default (state = [], action) => {
   Object.freeze(state);
   switch (action.type) {
-    case RECEIVE_ERRORS:
-      return action.errors;
+    // case RECEIVE_ERRORS:
+    //   return action.errors;
     default:
       return [];
   }
